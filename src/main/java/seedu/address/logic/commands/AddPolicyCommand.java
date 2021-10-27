14faf739e51da3f976e58a456bf5875a32858022
mokdarren <mokkwd@gmail.com>
2021-10-28 02:37:55 +0800

    AddPolicyCommand

--- src/main/java/seedu/address/logic/commands/AddPolicyCommand.java
+++ src/main/java/seedu/address/logic/commands/AddPolicyCommand.java
@@ -4,13 +4,27 @@ import static java.util.Objects.requireNonNull;
 
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.CollectionUtil;
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.Model;
+import seedu.address.model.person.Address;
 import seedu.address.model.person.Client;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
 import seedu.address.model.person.Policy;
+import seedu.address.model.person.Relationship;
+import seedu.address.model.person.Status;
+import seedu.address.model.tag.Tag;
 
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 /**
  * Adds a person to the address book.
@@ -53,10 +67,16 @@ public class AddPolicyCommand extends Command {
         if (!personToAddPolicy.getRelationship().value.equals("client")) {
             throw new CommandException(Messages.MESSAGE_INVALID_CLIENT_DISPLAYED_INDEX);
         }
-
-        if (!model.clientHasPolicy((Client) personToAddPolicy, policyToAdd)) {
+        Client clientToAddPolicy = (Client) personToAddPolicy;
+        if (!model.clientHasPolicy(clientToAddPolicy, policyToAdd)) {
             throw new CommandException(MESSAGE_DUPLICATE_POLICY);
         }
+
+        EditClientDescriptor editClientDescriptor = new EditClientDescriptor();
+
+
+        //actually an edit because person is immutable object
+        Client clientWithNewPolicy = createClientWithNewPolicy(clientToAddPolicy, editClientDescriptor);
 
         Policy newPolicy = new Policy(insurer, number, name, commission);
         Client editedClient = createEditedPerson(personToAddPolicy, newPolicy);
@@ -68,5 +88,99 @@ public class AddPolicyCommand extends Command {
         return other == this // short circuit if same object
                 || (other instanceof AddCommand // instanceof handles nulls
                 && toAdd.equals(((AddCommand) other).toAdd));
+    }
+
+    /**
+     * Creates and returns a {@code Person} with the details of {@code personToEdit}
+     * edited with {@code editPersonDescriptor}.
+     */
+    private static Client createClientWithNewPolicy(Client clientToEdit, EditClientDescriptor editClientDescriptor) {
+        assert clientToEdit != null;
+
+        Name updatedName = editClientDescriptor.getName().orElse(clientToEdit.getName());
+        Relationship updatedRelationship = editClientDescriptor.getRelationship()
+                .orElse(clientToEdit.getRelationship());
+        Phone updatedPhone = editClientDescriptor.getPhone().orElse(clientToEdit.getPhone());
+        Email updatedEmail = editClientDescriptor.getEmail().orElse(clientToEdit.getEmail());
+        Address updatedAddress = editClientDescriptor.getAddress().orElse(clientToEdit.getAddress());
+        Set<Tag> updatedTags = editClientDescriptor.getTags().orElse(clientToEdit.getTags());
+        Set<Policy> updatedPolicies = editClientDescriptor.getPolicies().orElse(clientToEdit.getPolicies());
+        Status status = editClientDescriptor.getStatus().orElse(clientToEdit.getStatus());
+        String notes = editClientDescriptor.getNotes().orElse(clientToEdit.getNotes());
+
+        return new Client(updatedName, updatedRelationship, updatedPhone, updatedEmail, updatedAddress, updatedTags);
+    }
+
+    /**
+     * Stores the details to edit the client with. Each non-empty field value will replace the
+     * corresponding field value of the client.
+     */
+    public static class EditClientDescriptor extends EditCommand.EditPersonDescriptor {
+        private Set<Policy> policies = new HashSet<>();
+        private Status status;
+        private String notes;
+        private LocalDateTime lastUpdated;
+
+        /**
+         * Creates editClientDescriptor to recreate immutable Clients
+         */
+        public EditClientDescriptor(EditClientDescriptor toCopy) {
+            setName(toCopy.name);
+            setRelationship(toCopy.relationship);
+            setPhone(toCopy.phone);
+            setEmail(toCopy.email);
+            setAddress(toCopy.address);
+            setTags(toCopy.tags);
+        }
+
+        /**
+         * Creates editClientDescriptor to recreate immutable Clients
+         */
+        public EditClientDescriptor() {
+            super();
+        }
+
+        public void setStatus(Status status) {
+            this.status = status;
+        }
+
+        public void setNotes(String notes) {
+            this.notes = notes;
+        }
+
+        public void setLastUpdated(LocalDateTime lastUpdated) {
+            this.lastUpdated = lastUpdated;
+        }
+
+        public Optional<Status> getStatus() {
+            return Optional.ofNullable(status);
+        }
+
+
+        public Optional<String> getNotes() {
+            return Optional.ofNullable(notes);
+        }
+
+        public Optional<LocalDateTime> getLastUpdated() {
+            return Optional.ofNullable(lastUpdated);
+        }
+
+        /**
+         * Sets {@code policies} to this object's {@code policies}.
+         * A defensive copy of {@code policies} is used internally.
+         */
+        public void setPolicies(Set<Policy> policies) {
+            this.policies = (policies != null) ? new HashSet<>(policies) : null;
+        }
+
+        /**
+         * Returns an unmodifiable policy set, which throws {@code UnsupportedOperationException}
+         * if modification is attempted.
+         * Returns {@code Optional#empty()} if {@code policis} is null.
+         */
+        public Optional<Set<Policy>> getPolicies() {
+            return (policies != null) ? Optional.of(Collections.unmodifiableSet(policies)) : Optional.empty();
+        }
+
     }
 }
---
+++ src/main/java/seedu/address/logic/parser/AddPolicyCommandParser.java
@@ -0,0 +1,70 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_RELATIONSHIP;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+
+import java.util.Set;
+import java.util.stream.Stream;
+
+import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Client;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
+import seedu.address.model.person.Relationship;
+import seedu.address.model.tag.Tag;
+
+/**
+ * Parses input arguments and creates a new AddCommand object
+ */
+public class AddPolicyCommandParser implements Parser<AddCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the AddCommand
+     * and returns an AddCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public AddCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_RELATIONSHIP, PREFIX_PHONE, PREFIX_EMAIL,
+                        PREFIX_ADDRESS, PREFIX_TAG);
+
+        if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_RELATIONSHIP, PREFIX_PHONE, PREFIX_EMAIL,
+                PREFIX_ADDRESS)
+                || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
+        }
+
+        Name name = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get());
+        Relationship relationship = ParserUtil.parseRelationship(argMultimap.getValue(PREFIX_RELATIONSHIP).get());
+        Phone phone = ParserUtil.parsePhone(argMultimap.getValue(PREFIX_PHONE).get());
+        Email email = ParserUtil.parseEmail(argMultimap.getValue(PREFIX_EMAIL).get());
+        Address address = ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS).get());
+        Set<Tag> tagList = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
+
+        if (relationship.toString().equals("client")) {
+            Client person = new Client(name, relationship, phone, email, address, tagList);
+            return new AddCommand(person);
+        } else {
+            Person person = new Person(name, relationship, phone, email, address, tagList);
+            return new AddCommand(person);
+        }
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
+
+}
